4eec7703c5f31a53e026308af09b4b73bceeae1d
Yelizaveta Ivanchenko <yelizaveta.ivanchenko@cloudreach.com>
2022-11-09 09:46:37 +0100

    finished Packer Build AMI for Kibana Server

--- infrastructure/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "group-3-talent-academy-elk-project"
-    key    = "kibana-ami/terraform.tfstates"
-    dynamodb_table = "terraform-lock"
-  }
-}
--- infrastructure/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ec2_public_ip" {
-    value = aws_instance.web_server.public_ip
-}
